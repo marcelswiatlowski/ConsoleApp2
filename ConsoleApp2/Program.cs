4d172184623dc44502476c430f5b5818427b2b09
3120235 <3120235@TE-GDA-D-D12C01>
2022-03-11 13:09:19 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,62 @@
+﻿    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            int min = 0;
+            int max = 10;
+            int guess; ;
+            int number;
+            int guesses;
+            String response;
+
+            while (playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                response = "";
+                number = random.Next(min, max + 1);
+            
+                while (guess != number)
+                {
+                    Console.WriteLine("Zgadnij liczbe między" + min + " - " + max + "");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess:" + guess);
+
+                    if(guess > number)
+                    {
+                    Console.WriteLine(guess + " jest za wysoka!");                    
+                    }
+                    else if( guess < number)
+                    {
+                    Console.WriteLine(guess + " jest za niska!");
+                    }
+                    guesses++;
+                }
+                Console.WriteLine("Numer:" + number);
+                Console.WriteLine("Wygrałeś!");
+                Console.WriteLine("Twoje domysły: " + guesses);
+                Console.WriteLine("Chciałbyś zagrać jeszcze raz (T/N): ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+                
+                if(response == "T")
+                {
+                     playAgain = true;
+                }
+                else
+                {
+                    playAgain = false;
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dzięki za grę!");
+
+            Console.ReadKey();
+        }
+
+    }
+
